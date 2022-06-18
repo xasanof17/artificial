b41ef5a8846fadffd845117c3560f91f65eaf301
javohir <xasanof17@gmail.com>
2022-06-18 20:55:42 +0500

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { NavWrapper } from './navbarStyled'
+
+const Navbar = () => {
+  return (
+    <NavWrapper>Navbar</NavWrapper>
+  )
+}
+
+export default Navbar
---
+++ src/components/Navbar/navbarStyled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const NavWrapper = styled.nav`
+padding: 20px 0;
+position: sticky;
+top: 0;
+left: 0;
+width: 100%;
+`;
